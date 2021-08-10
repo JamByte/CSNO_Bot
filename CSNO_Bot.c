e3be5976bf6dc1b49e86343848a85ac46028ba26
JamByte <jambyte1@gmail.com>
2021-08-10 16:06:43 +0100

    Update CSNO_Bot.c

--- CSNO_Bot.c
+++ CSNO_Bot.c
@@ -6,6 +6,12 @@
 #include "discord.h"
 #include "cee-utils.h" /* cee_timestamp_ms() */
 
+long long unsigned int authors[];
+long long unsigned int channels[];
+int index;
+#define CHARSTOCOPY 50;
+
+
 void on_ready(struct discord *client, const struct discord_user *bot) {
   log_info("CSNO_Bot succesfully connected to Discord as %s#%s!",
       bot->username, bot->discriminator);
@@ -28,14 +34,46 @@ void sendembed(struct discord *client, const struct discord_user *bot, const str
   discord_embed_cleanup(&embed);
 }
 void on_message(struct discord *client, const struct discord_user *bot, const struct discord_message *msg) {
-  if (0 == strcmp(msg->content, "|ping")) {
-    char str[512];
-    snprintf(str, sizeof str,"Pong <@%lld>", msg->author->id);
-    sendembed(client,bot,msg, str);
-  }
+   
+    if(msg.content == 0) {return;}
+    if(msg->content == '\0') {return;}
+    if(strstr(msg,"https://") == 0){return;}
+    authors[index] = msg->author.id;
+    channels[index = msg.channel_id;
+    if(index >49){
+        index =0;
+    }
+    long long unsigned int channel1=0;
+    long long unsigned int channel2=0;
+    for(i=0; i<50; i++){
+        if(authors[i] == msg->author.id){
+            if(channel1 == 0 || channel1 == channels[i] ){
+                channel1 = channels[i];
+                continue;
+            }
+            else if(channel2 == 0|| channel2== channels[i]){
+                channel1 = channels[i];
+                continue;
+            }
+            else
+            {
+                //ban them !!!
+                sendembed(client, bot,msg,"Pow you would of been banned")
+            }
+        }
+    }
+
 }
 int main(int argc, char *argv[])
 {
+ 
+    authors = malloc(50*sizeof(long long unsigned int)):
+
+    
+    channels = malloc(50*sizeof(long long unsigned int)):
+
+
+    index=0;
   time_t t;
   srand((unsigned) time(&t));
   const char *config_file;

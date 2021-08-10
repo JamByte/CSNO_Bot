bf5d29f49aa3735369a576183550651ab42406bb
JamByte <jambyte1@gmail.com>
2021-08-10 14:41:58 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-cp CSNO_Bot.c ../CSNO_Bot.c
 cd ..
 cd ..
-make bots
+cc -std=c11 -O0 -g -Wall -Wno-unused-function -I. -I./cee-utils -I./common -I./common/third-party -DLOG_USE_COLOR -Wno-unused-but-set-variable -fPIC -D_XOPEN_SOURCE=700 -I./mujs -o bots/CSNO_Bot/CSNO_Bot.exe bots/CSNO_Bot/CSNO_Bot.c -ldiscord -lgithub -lreddit -lslack -L./lib -lpthread -lm  -lcurl -lcrypto
 cd bots 
-cp CSNO_Bot.exe CSNO_Bot/CSNO_Bot.exe
+cp bot.config CSNO_Bot/bot.config
+cd CSNO_Bot
